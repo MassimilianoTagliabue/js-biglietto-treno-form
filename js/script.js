8265aeefab1ea5fe9b6d9195a2ebd89295f8cb07
MassimilianoTagliabue <massimilianotagliabue.04@gmail.com>
2024-11-09 20:59:47 +0100

    feat: layout biglietto

--- js/script.js
+++ js/script.js
@@ -2,14 +2,29 @@
 const chilometriInput = document.getElementById("chilometri");
 const anniInput = document.getElementById("anni");
 const butInput = document.getElementById("conferma");
-const formElem = document.getElementById("form")
+const formElem = document.getElementById("form");
+const nomeInput = document.getElementById("nome");
+
+//card
+const nomeCard = document.getElementById("nome-card");
+const prezzoCard = document.getElementById("prezzo-card");
+const numeroTrenoCard = document.getElementById("numero-treno-card");
+const carrozzaCard = document.getElementById("carrozza-card");
 
 
 
 //esecuzione logica
 
+const numeroTrenoRandom = Math.floor(Math.random() * 1000000 + 1) ;
+
+
+const numeroCarrozzaRandom = Math.floor(Math.random() * 10 + 1) ;
+
+
+
 formElem.addEventListener("submit", function (event) {
 
+    
     
     
     event.preventDefault();
@@ -58,4 +73,18 @@ formElem.addEventListener("submit", function (event) {
     
     }
 
+
+
+    nomeCard.innerHTML = nomeInput.value.trim();
+    prezzoCard.innerHTML = `${prezzoTotale}£`;
+    numeroTrenoCard.innerHTML = `${numeroTrenoRandom}`;
+    carrozzaCard.innerHTML = `${numeroCarrozzaRandom}`;
+
+
 })
+
+
+
+
+
+
